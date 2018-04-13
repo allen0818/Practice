ee23f39ded4a696d317e3d06b134efb0ca6d6edc
ct_hu <ct_hu@fiberlogic.com>
2018-04-13 19:33:11 +0800

    first commit

---
+++ C#/DelegatePrac/DelegatePrac/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DelegatePrac1
+{
+    class Cat
+    {
+        private string name;
+        public Cat(string name)
+        {
+            this.name = name;
+        }
+
+        public delegate void CatShoutEventHandler(object sender, CatShoutEventArgs args);
+        public event CatShoutEventHandler CatShout;
+
+        public void Shout()
+        {
+            Console.WriteLine("喵, 我是{0}", name);
+            if (CatShout != null) //指的是是否有人註冊了這個Event
+            {
+                CatShoutEventArgs e = new CatShoutEventArgs();
+                e.Name = this.name;
+
+                CatShout(this, e);
+            }
+        }
+    }
+}
---
+++ C#/DelegatePrac/DelegatePrac/CatShoutEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DelegatePrac1
+{
+    class CatShoutEventArgs : EventArgs
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
